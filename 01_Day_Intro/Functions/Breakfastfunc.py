ed98b5c535fe2a3c40142e73dbc0c1f6ceaa92bc
Shankar Hariharan K <52159502+devshank3@users.noreply.github.com>
2025-07-27 00:57:50 +0530

    Add introductory Python function and syntax examples
    Added new files demonstrating basic Python functions, function reuse, and essential syntax. These examples cover simple breakfast preparation functions and basic operations/types for beginners.

---
+++ 01_Day_Intro/Functions/Breakfastfunc.py
@@ -0,0 +1,26 @@
+""" A Functional Breakfast """
+
+print('Mixing the ingredients')
+print('Pouring the mixture into a frying pan')
+print('Cooking the first side')
+print('Flipping it!')
+print('Cooking the other side\n')
+
+omelette = 'a tasty omelette'
+
+""" A Functional Breakfast """
+
+def make_omelette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side\n')
+    omelette = 'a tasty omelette'
+    return omelette
+
+# make breakfast for two
+barron_breakfast = make_omelette()
+olivia_breakfast = make_omelette()
+print(f'Barron is having {barron_breakfast}\n')
+print(f'Olivia is having {olivia_breakfast}\n')
