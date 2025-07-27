42cd5cbf75771770b74da98bf0ad01fd547ebfde
Shankar Hariharan K <52159502+devshank3@users.noreply.github.com>
2025-07-28 00:28:48 +0530

    Add fancy omelette function and global scope example
    Introduced make_fancy_omelette to accept variable ingredients and updated Barron's breakfast to use it. Added loc_glbl.py to demonstrate local and global variable scope in breakfast functions.

--- 02_Day_More_Functions/func_reuse.py
+++ 02_Day_More_Functions/func_reuse.py
@@ -41,8 +41,13 @@ def make_omelette(ingredients=None):
     omelette = "A tasty omelette made with " + (ingredients if ingredients else "standard ingredients")
     return omelette
 
+#takes in tuple arguments
+def make_fancy_omelette(*ingredients):
+    mix_and_cook()
+    omelette = "A fancy omelette with " + ", ".join(ingredients)
+    return omelette
 
-barron_breakfast = make_omelette("spinach and cheese")
+barron_breakfast = make_fancy_omelette("smoked salmon", "cream cheese", "chives")
 olivia_breakfast = make_pancake("blueberries")
 
 print(f'Barron is having {barron_breakfast}\n')
---
+++ 02_Day_More_Functions/loc_glbl.py
@@ -0,0 +1,37 @@
+cheese = 'cheddar'
+
+# parameter and variables created inside the function will be in local scope
+
+""" A Functional Breakfast """
+
+cheese = 'cheddar'
+
+def mix_and_cook():
+    print('Mixing the ingredients')
+    print('Greasing the frying pan')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side\n')
+
+def make_omelette():
+    global cheese
+    cheese = 'swiss'
+    mix_and_cook()
+    omelette = f'a {cheese} omelette'
+    return omelette
+
+def make_pancake():
+    mix_and_cook()
+    pancake = f'a {cheese} pancake'
+    return pancake
+
+# make breakfast for two
+print(f'*** global cheese is {cheese} ***\n')
+barron_breakfast = make_pancake()
+print(f'*** global cheese is {cheese} ***\n')
+olivia_breakfast = make_omelette()
+print(f'*** global cheese is {cheese} ***\n')
+
+print(f'Barron is having {barron_breakfast}\n')
+print(f'Olivia is having {olivia_breakfast}\n')
